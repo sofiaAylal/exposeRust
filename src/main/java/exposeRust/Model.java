a63e1d2ec3409bbca948681a77ef5de5d0531439
nicolas <nicolas.trenchs@etu.unilim.fr>
2024-03-14 11:04:30 +0100

    ajout de la classe ViewModel avec fonction updateData (appel du setteur du Model

--- src/main/java/exposeRust/Model.java
+++ src/main/java/exposeRust/Model.java
@@ -3,4 +3,12 @@ package exposeRust;
 public class Model {
 	private int data;
 
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
 }
---
+++ src/main/java/exposeRust/ViewModel.java
@@ -0,0 +1,14 @@
+package exposeRust;
+
+public class ViewModel {
+	private Model model;
+	
+	public ViewModel(Model model) {
+		this.model = model;
+	}
+	
+	public void updateData(int data) {
+		model.setData(data);
+	}
+
+}
